9c8783b9c323fa1bf13659247498d538cba34c5c
Ismail Hossain <>
2021-04-23 10:44:11 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+#Deguging
+
+# print("Day 1 - String Manipulation")
+# print('String Concatenation is done with the "+" sign')
+# print('e.g print("Hello" + "world")')
+# print("New lines can be created with a backslash and n.")
+
+# Comment shortcut = Ctrl+/
+
+#Input Function
+
+# name = input("What is your name? ")
+# length = len(name)
+# print(length)
+
+
+a = input("a: ")
+b = input("b: ")
+
+print("a = " + a)
+print("b = " + b)
+
+
